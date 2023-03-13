2dd81104c1111ac529e5091514d5e558c7e4c397
NguyenXuanCanh <x.canh040701@gmail.com>
2023-03-13 22:26:22 +0700

    update

--- src/components/NotiItem.js
+++ src/components/NotiItem.js
@@ -1,59 +1,64 @@
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Text, themeColor } from "react-native-rapi-ui";
 import { Col, Row } from "./Flex";
 
-export default function NotiItem() {
+export default function NotiItem(props) {
+  const { title, date, time, callback } = props;
   return (
-    <View style={styles.notiItem}>
+    <TouchableOpacity
+      style={styles.notiItem}
+      onPress={() => {
+        callback && callback();
+      }}
+    >
       <View style={{ padding: 5 }}>
-        <Text size="sm">
+        <Text size="lg">
           The status of package with id #012312311 has changed to{" "}
-          <Text status="warning" size="sm">
+          <Text status="warning" size="lg">
             Waiting for Delivery
           </Text>
         </Text>
       </View>
       <View
         style={{
-          borderTopWidth: 1,
-          borderStyle: "dashed",
           padding: 4,
-          borderColor: themeColor.gray300,
+          //   justifyContent: "flex-end",
+          marginTop: "auto",
         }}
       >
         <Row>
           <Col numRows={1}>
-            <Text style={{ color: themeColor.gray300 }} size="sm">
+            <Text style={{ color: themeColor.gray300 }} size="lg">
               13/02/2023
             </Text>
           </Col>
           <Col numRows={1}>
             <Text
               style={{ textAlign: "right", color: themeColor.gray300 }}
-              size="sm"
+              size="lg"
             >
               17:32 PM
             </Text>
           </Col>
         </Row>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   notiItem: {
-    borderColor: themeColor.black,
-    borderWidth: 1,
+    minHeight: 110,
     marginTop: 5,
     padding: 5,
     borderRadius: 8,
-    borderColor: "#A19CFF",
     borderLeftWidth: 1.5,
     borderTopWidth: 1.5,
     borderRightWidth: 5,
     borderBottomWidth: 5,
+    backgroundColor: themeColor.white,
+    paddingBottom: 10,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { View, Linking, KeyboardAvoidingView, StatusBar } from "react-native";
+import {
+  View,
+  KeyboardAvoidingView,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 import { getAuth, signOut } from "firebase/auth";
 import {
   Layout,
@@ -20,42 +25,82 @@ import { useRecoilState } from "recoil";
 import ModalClockIn from "../components/ModalClockIn";
 import Loading from "./utils/Loading";
 import { BASE_URL } from "../config/constants";
+import NotiItem from "../components/NotiItem";
 
 export default function ({ navigation }) {
-  const { isDarkmode, setTheme } = useTheme();
-  const [searchText, setSearchText] = useState("");
+  const { isDarkmode } = useTheme();
   const auth = getAuth();
   const [isClockedIn, setIsClockedIn] = useRecoilState(isClockIn);
   const [isVisible, setVisible] = useState(true);
-  const [loading, setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     (async () => {
-        async function fetchData() {
-            const response = await axios.get(`${BASE_URL}/clockin_status/${auth.currentUser.uid}`);
-            return response;
-        }
-        if(auth.currentUser){
-            fetchData()
-        .then((response)=>{
-            console.log(response.data.data)
-            setIsClockedIn(res.data)
-            setLoading(false)
-        }).catch((error)=>{
-            console.log(error)
-        });
-        }
-        setLoading(false);
+      async function fetchData() {
+        const response = await axios.get(
+          `${BASE_URL}/clockin_status/${auth.currentUser.uid}`
+        );
+        return response;
+      }
+      if (auth.currentUser) {
+        fetchData()
+          .then((response) => {
+            console.log(response.data.data);
+            setIsClockedIn(res.data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      setLoading(false);
     })();
   }, []);
 
-  useEffect(()=>{
-    if(isClockedIn){
-        console.log(isClockedIn)
-        setVisible(false);
+  useEffect(() => {
+    if (isClockedIn) {
+      console.log(isClockedIn);
+      setVisible(false);
     }
-  },[isClockedIn])
+  }, [isClockedIn]);
+
+  const onClockIn = () => {
+    setLoading(true);
+    (async () => {
+      async function fetchData() {
+        const response = await axios.get(
+          `${BASE_URL}/generate_trip/${auth.currentUser.uid}`
+        );
+        return response;
+      }
+      async function updateClockinStatus() {
+        const response = await axios.get(
+          `${BASE_URL}/update_clockin_status/${auth.currentUser.uid}/1`
+        );
+        return response;
+      }
+      updateClockinStatus()
+        .then((res) => {
+          res.data &&
+            fetchData()
+              .then((response) => {
+                console.log(response.data);
+                setIsClockedIn(true);
+                setLoading(false);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      //   setIsClockedIn(true);
+      //   setLoading(false);
+    })();
+  };
 
   return (
     <KeyboardAvoidingView behavior="height" enabled style={{ flex: 1 }}>
@@ -85,187 +130,251 @@ export default function ({ navigation }) {
           backgroundColor="#A19CFF"
           borderColor="#A19CFF"
         />
-        {loading ? 
-        <Loading/>
-        : 
-        <>
-        <ModalClockIn isVisible={isVisible} onClose={()=>{setVisible(false)}} />
-        <View
-          style={{
-            flex: 2,
-            alignItems: "center",
-            paddingTop: 20,
-            backgroundColor: "#A19CFF",
-            //   justifyContent: "center",
-            //   backgroundColor: "black"
-          }}
-        >
-          <Text
-            fontWeight="light"
-            style={{ textAlign: "center", color: themeColor.white }}
-            size="sm"
-          >
-            This app is working in
-          </Text>
-          <Text
-            fontWeight="bold"
-            style={{ textAlign: "center", marginTop: 10 }}
-            size="md"
-          >
-            Ho Chi Minh City
-            <Ionicons name="location-outline" size={20} />
-          </Text>
-          <Text
-            fontWeight="bold"
-            style={{ textAlign: "center", marginTop: 50 }}
-            size="xl"
-          >
-            Track Your Package
-          </Text>
-          <View style={{ width: "80%", marginTop: 20 }}>
-            <TextInput
-              placeholder="Enter your package number"
-              leftContent={
-                <>
-                  <Ionicons name="cube-outline" size={20} />
-                </>
-              }
-              value={searchText}
-              onChangeText={(val) => setSearchText(val)}
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <ModalClockIn
+              isVisible={isVisible}
+              onClose={() => {
+                setVisible(false);
+              }}
+              isClockIn={isClockedIn}
+              onClockIn={onClockIn}
             />
-          </View>
-        </View>
-        <View
-          style={{
-            flex: 3,
-            alignItems: "center",
-            width: "100%",
-            backgroundColor: "#A19CFF",
-          }}
-        >
-          <Section
-            style={{
-              marginTop: "auto",
-              padding: 0,
-              width: "100%",
-              minHeight: 300,
-              borderTopLeftRadius: 25,
-              borderTopEndRadius: 25,
-            }}
-          >
-            <SectionContent style={{ padding: 0, width: "100%" }}>
-              <Row style={{ height: "50%" }}>
-                <Col numRows={7}>
-                  <Button
-                    style={{
-                      marginTop: 10,
-                      height: "90%",
-                      borderLeftWidth: 1.5,
-                      borderTopWidth: 1.5,
-                      borderRightWidth: 5,
-                      borderBottomWidth: 5,
-                    }}
-                    status="info"
-                    onPress={() => {
-                      navigation.navigate("Map");
-                    }}
-                    outline
-                    text={
-                      <>
-                        <Text size="xl" fontWeight="bold">
-                          My Work Map
-                        </Text>{"             "}
-                        <Text size="sm">Ex ea Lorem deserunt ipsum qui.</Text>
-                      </>
-                    }
-                  />
-                </Col>
-                <Col numRows={1}></Col>
-                <Col numRows={7}>
-                  <Button
-                    text={
-                      <>
-                        <Text size="xl" fontWeight="bold">
-                          History
-                        </Text>{" "}
-                        <Text size="sm">Ex ea Lorem deserunt ipsum qui.</Text>
-                      </>
-                    }
-                    onPress={() => {
-                      navigation.navigate("History");
-                    }}
-                    outline
-                    style={{
-                      marginTop: 10,
-                      height: "90%",
-                      borderLeftWidth: 1.5,
-                      borderTopWidth: 1.5,
-                      borderRightWidth: 5,
-                      borderBottomWidth: 5,
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row style={{ height: "50%" }}>
-                <Col numRows={7}>
-                  <Button
-                    style={{
-                      marginTop: 10,
-                      height: "90%",
-                      borderLeftWidth: 1.5,
-                      borderTopWidth: 1.5,
-                      borderRightWidth: 5,
-                      borderBottomWidth: 5,
-                    }}
-                    outline
-                    text={
-                      <>
-                        <Text size="xl" fontWeight="bold">
-                          My Vehicle
-                        </Text>{" "}
-                        <Text size="sm">Ex ea Lorem deserunt ipsum qui.</Text>
-                      </>
-                    }
-                    status="danger"
-                    onPress={() => {
-                      navigation.navigate("Vehicle");
-                    }}
-                  />
-                </Col>
-                <Col numRows={1}></Col>
-                <Col numRows={7}>
-                  <Button
-                    text={
-                      <>
-                        <Text size="xl" fontWeight="bold">
-                          My Contact
-                        </Text>
-                        {"     "}
-                        <Text size="sm">Ex ea Lorem deserunt ipsum qui.</Text>
-                      </>
-                    }
-                    onPress={() => {
-                      navigation.navigate("Profile");
-                    }}
-                    outline
-                    style={{
-                      marginTop: 10,
-                      height: "90%",
-                      borderLeftWidth: 1.5,
-                      borderTopWidth: 1.5,
-                      borderRightWidth: 5,
-                      borderBottomWidth: 5,
-                    }}
-                    status="warning"
-                  />
-                </Col>
-              </Row>
-            </SectionContent>
-          </Section>
-        </View>
-        </>
-        }
-        
+            <View
+              style={{
+                flex: 2,
+                alignItems: "center",
+                paddingTop: 20,
+                backgroundColor: "#A19CFF",
+                //   justifyContent: "center",
+                //   backgroundColor: "black"
+              }}
+            >
+              <Text
+                fontWeight="light"
+                style={{ textAlign: "center", color: themeColor.white }}
+                size="sm"
+              >
+                This app is working in
+              </Text>
+              <Text
+                fontWeight="bold"
+                style={{ textAlign: "center", marginTop: 10 }}
+                size="md"
+              >
+                Ho Chi Minh City
+                <Ionicons name="location-outline" size={20} />
+              </Text>
+              <Text
+                fontWeight="bold"
+                style={{
+                  textAlign: "center",
+                  marginTop: 50,
+                  color: themeColor.white,
+                }}
+                size="xl"
+              >
+                Notifications
+              </Text>
+              <ScrollView style={{ width: "80%", marginTop: 20 }}>
+                {/* load max 3 */}
+                <NotiItem style={{ backgroundColor: "white" }} />
+                {/* <NotiItem style={{ backgroundColor: "white" }} /> */}
+                {/* <NotiItem style={{ backgroundColor: "white" }} /> */}
+              </ScrollView>
+            </View>
+            <View
+              style={{
+                flex: 2,
+                alignItems: "center",
+                width: "100%",
+                backgroundColor: "#A19CFF",
+              }}
+            >
+              <Section
+                style={{
+                  marginTop: "auto",
+                  padding: 0,
+                  width: "100%",
+                  minHeight: 300,
+                  borderTopLeftRadius: 25,
+                  borderTopEndRadius: 25,
+                }}
+              >
+                <ScrollView
+                  style={{ padding: 20, width: "100%", height: "100%" }}
+                >
+                  <Row style={{ height: 150 }}>
+                    <Col numRows={7}>
+                      <Button
+                        style={{
+                          marginTop: 10,
+                          height: "90%",
+                          borderLeftWidth: 1.5,
+                          borderTopWidth: 1.5,
+                          borderRightWidth: 5,
+                          borderBottomWidth: 5,
+                        }}
+                        status="info"
+                        onPress={() => {
+                          navigation.navigate("Map");
+                        }}
+                        outline
+                        leftContent={
+                          <Ionicons
+                            name="map-outline"
+                            size={20}
+                            color={themeColor.black}
+                          />
+                        }
+                        text={
+                          <>
+                            <Text size="xl" fontWeight="bold">
+                              My Work Map
+                            </Text>
+                          </>
+                        }
+                      />
+                    </Col>
+                    <Col numRows={1}></Col>
+                    <Col numRows={7}>
+                      <Button
+                        text={
+                          <>
+                            <Text size="xl" fontWeight="bold">
+                              History
+                            </Text>
+                          </>
+                        }
+                        leftContent={
+                          <Ionicons
+                            name="clipboard-outline"
+                            size={20}
+                            color={themeColor.black}
+                          />
+                        }
+                        onPress={() => {
+                          navigation.navigate("History");
+                        }}
+                        outline
+                        style={{
+                          marginTop: 10,
+                          height: "90%",
+                          borderLeftWidth: 1.5,
+                          borderTopWidth: 1.5,
+                          borderRightWidth: 5,
+                          borderBottomWidth: 5,
+                        }}
+                      />
+                    </Col>
+                  </Row>
+                  <Row style={{ height: 150 }}>
+                    <Col numRows={7}>
+                      <Button
+                        style={{
+                          marginTop: 10,
+                          height: "90%",
+                          borderLeftWidth: 1.5,
+                          borderTopWidth: 1.5,
+                          borderRightWidth: 5,
+                          borderBottomWidth: 5,
+                        }}
+                        leftContent={
+                          <Ionicons
+                            name="bus-outline"
+                            size={20}
+                            color={themeColor.black}
+                          />
+                        }
+                        outline
+                        text={
+                          <>
+                            <Text size="xl" fontWeight="bold">
+                              My Vehicle
+                            </Text>
+                          </>
+                        }
+                        status="success"
+                        onPress={() => {
+                          navigation.navigate("Vehicle");
+                        }}
+                      />
+                    </Col>
+                    <Col numRows={1}></Col>
+                    <Col numRows={7}>
+                      <Button
+                        text={
+                          <>
+                            <Text size="xl" fontWeight="bold">
+                              My Contact
+                            </Text>
+                          </>
+                        }
+                        leftContent={
+                          <Ionicons
+                            name="information-circle-outline"
+                            size={20}
+                            color={themeColor.black}
+                          />
+                        }
+                        onPress={() => {
+                          navigation.navigate("Profile");
+                        }}
+                        outline
+                        style={{
+                          marginTop: 10,
+                          height: "90%",
+                          borderLeftWidth: 1.5,
+                          borderTopWidth: 1.5,
+                          borderRightWidth: 5,
+                          borderBottomWidth: 5,
+                        }}
+                        status="warning"
+                      />
+                    </Col>
+                  </Row>
+                  <Row style={{ height: 150 }}>
+                    <Col numRows={7}>
+                      <Button
+                        style={{
+                          marginTop: 10,
+                          height: "90%",
+                          borderLeftWidth: 1.5,
+                          borderTopWidth: 1.5,
+                          borderRightWidth: 5,
+                          borderBottomWidth: 5,
+                        }}
+                        leftContent={
+                          <Ionicons
+                            name="time-outline"
+                            size={20}
+                            color={themeColor.black}
+                          />
+                        }
+                        outline
+                        text={
+                          <>
+                            <Text size="xl" fontWeight="bold">
+                              Work Status
+                            </Text>
+                          </>
+                        }
+                        status="danger"
+                        onPress={() => {
+                          setVisible(true);
+                        }}
+                      />
+                    </Col>
+                    <Col numRows={1}></Col>
+                    <Col numRows={7}></Col>
+                  </Row>
+                </ScrollView>
+              </Section>
+            </View>
+          </>
+        )}
       </Layout>
     </KeyboardAvoidingView>
   );
